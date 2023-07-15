b831bfeaf63fd09e138a5a561622dd8a3d2557e6
yanxinle <1020121123@qq.com>
2023-07-15 17:04:43 +0800

    重构提示字符

--- game/guess_game第2版.py
+++ game/guess_game第2版.py
@@ -1,24 +1,29 @@
 import random
 
+from comm.common import tuichu
+
+
+def tiShiFu(key):
+    prefix = "---------"
+    return (prefix + key + prefix)
+
 
 def guess_game_ShengJiBan():
     number = random.randint(1, 100)
     print('现在是一个猜数游戏的升级版!')
     while True:
         huoqu_str = input('请输入一个1到100的数 (输入q退出) : ')
-        if huoqu_str == 'q':
-            print("已退出猜数游戏")
-            break
+        tuichu(huoqu_str)
         try:
             huoqu = int(huoqu_str)
             if huoqu > number + 10:
-                print("\n---------太大了---------\n这个数好大呀！加油，请继续猜\n")
+                print(tiShiFu("太大了") + "这个数好大呀！加油，请继续猜\n")
             elif number + 10 >= huoqu > number:
-                print("\n---------接近了---------\n在努力一点\n")
+                print(tiShiFu("接近了") + "在努力一点\n")
             elif number - 10 <= huoqu < number:
-                print("\n---------接近了---------\n在努力一点\n")
+                print(tiShiFu("接近了") + "在努力一点\n")
             elif huoqu < number - 5:
-                print("\n---------太小了---------\n这个数好小呀！加油，你一定可以猜中的\n")
+                print(tiShiFu("太小了") + "这个数好小呀！加油，你一定可以猜中的\n")
             else:
                 print("恭喜你！猜对了！新的一局开始了")
         except:
