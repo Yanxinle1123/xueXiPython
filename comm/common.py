95a8a7ebdcfbeaa897c7bb48517e2aa58d67ca6a
yanxinle <1020121123@qq.com>
2023-07-15 18:07:09 +0800

    增加出文字的效果

--- comm/common.py
+++ comm/common.py
@@ -1,7 +1,22 @@
 import sys
+import time
 
 
 def tuichu(guess_str, tishi='已退出'):
     if guess_str == 'q':
         print(tishi)
         sys.exit()
+
+
+def slow_print(text, delay=0.25):
+    for char in text:
+        print(char, end='', flush=True)
+        time.sleep(delay)
+    print()  # 换行
+
+
+def slow_input(text, delay=0.25):
+    for char in text:
+        print(char, end='', flush=True)
+        time.sleep(delay)
+    return input()  # 换行
---
+++ other/慢慢出来.py
@@ -0,0 +1,12 @@
+import time
+
+
+def slow_print(text, delay=0.25):
+    for char in text:
+        print(char, end='', flush=True)
+        time.sleep(delay)
+    print()  # 换行
+
+
+text = "Hello, I am an AI language model."
+slow_print(text)
