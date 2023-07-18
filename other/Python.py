7859b61ce0a31f570f3b2cdc7e3d4f3a48669beb
yanxinle <1020121123@qq.com>
2023-07-18 13:43:39 +0800

    更新 Python 程序

--- other/Python.py
+++ other/Python.py
@@ -85,10 +85,8 @@ def python():
                     }
                     while True:
                         is_quit = input("现在，新的一局开始了！(是否要退出？输入tc退出，输入其他继续)")
-                        if is_quit == 'tc' or is_quit == 'tuichu' or is_quit == '退出':
+                        if is_quit == 'tc':
                             print("已退出石头剪刀布游戏")
-                            break
-                        if is_quit == 'QiangZhiTuiChu' or is_quit == '强制退出':
                             break
                         player_score = 0
                         computer_score = 0
@@ -376,9 +374,9 @@ def python():
 
         elif huoqu == 'calc':
             print(
-                '1. 计算器\n2. 日期计算器\n3. 计算水仙花数\n4. 计算是否是闰年\n5. 计算是否构成三角形\n6. 温度转换\n7. 年龄计算器')
+                '1. 计算器\n2. 日期计算器\n3. 计算水仙花数\n4. 计算是否是闰年\n5. 计算是否构成三角形\n6. 温度转换\n7. 年龄计算器\n8. 货币转换')
             shuru = input('请选择(输入数字)>>>')
-            if shuru != '1' and shuru != '2' and shuru != '3' and shuru != '4' and shuru != '5' and shuru != '6' and shuru != '7':
+            if shuru != '1' and shuru != '2' and shuru != '3' and shuru != '4' and shuru != '5' and shuru != '6' and shuru != '7' and shuru != '8':
                 print('输入有误')
             elif shuru == '1':
                 while True:
@@ -525,6 +523,48 @@ def python():
                             print("输入有误")
                     except:
                         print("输入有误")
+            elif shuru == '8':
+                def huo_bi_zhuan_huan():
+                    print('这是一个可以转换货币的程序')
+                    while True:
+                        try:
+                            huoqu = input(
+                                '1.美元转人名币\n2.人民币转美元\n3.港币转人名币\n4.人名币转港币\n5.美元转港币\n6.港币转美元\n请选择转换方式(输入数字,输入q退出):')
+                            if huoqu.lower() == 'q':
+                                print('已退出')
+                                break
+                            elif huoqu == '1':
+                                shuru = float(input('请输入美元:'))
+                                jieguo = shuru * 6.48
+                                print('对应的人名币为:', jieguo)
+                            elif huoqu == '2':
+                                shuru = float(input('请输入人名币:'))
+                                jieguo = shuru / 6.48
+                                print('对应的美元为:', jieguo)
+                            elif huoqu == '3':
+                                shuru = float(input('请输入港币:'))
+                                jieguo = shuru * 0.83
+                                print('对应的人名币为:', jieguo)
+                            elif huoqu == '4':
+                                shuru = float(input('请输入人名币:'))
+                                jieguo = shuru / 0.83
+                                print('对应的港币为:', jieguo)
+                            elif huoqu == '5':
+                                shuru = float(input('请输入美元:'))
+                                jieguo = shuru / 0.128
+                                print('对应的港币为:', jieguo)
+                            elif huoqu == '6':
+                                shuru = float(input('请输入港币:'))
+                                jieguo = shuru * 0.128
+                                print('对应的美元为:', jieguo)
+                            else:
+                                print(huoqu, '不是一个选择')
+                        except:
+                            print('输入有误')
+
+                if __name__ == '__main__':
+                    huo_bi_zhuan_huan()
+
         elif huoqu == 'area':
             while True:
                 huoqu = input(
