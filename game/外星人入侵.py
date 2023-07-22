6856ebcf170badfbfcb6c622e072ab8edd8da7ff
yanxinle <1020121123@qq.com>
2023-07-22 18:50:35 +0800

    增加外星人按钮代码

--- game/外星人入侵.py
+++ game/外星人入侵.py
@@ -8,6 +8,7 @@ import settings
 import ship
 from alien import Alien
 from bullet import Bullet
+from game.button import Button
 from game_stats import GameStats
 
 
@@ -18,7 +19,8 @@ class AlienInvasion:
         self.clock = pygame.time.Clock()
         self.settings = settings.Settings()
 
-        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.screen = pygame.display.set_mode((1200, 800))
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         self.settings.to_string()
@@ -34,7 +36,9 @@ class AlienInvasion:
 
         # 创建统计
         self.stats = GameStats(self)
-        self.game_active = True
+        self.game_active = False
+
+        self.play_button = Button(self, "Play Game")
 
     def _ship_hit(self):
         if self.stats.ships_left > 0:
@@ -48,6 +52,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.game_active = False
+            pygame.mouse.set_visible(True)
 
     def _check_aliens_bottom(self):
         for alien in self.aliens.sprites():
@@ -128,6 +133,9 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
 
     def _check_keydown_events(self, event):
         if event.type == pygame.KEYDOWN:
@@ -164,7 +172,23 @@ class AlienInvasion:
             bullet.draw_bullet()
         self.ship.blitme()
         self.aliens.draw(self.screen)
+
+        if not self.game_active:
+            self.play_button.draw_button()
         pygame.display.flip()
+
+    def _check_play_button(self, mouse_pos):
+        if self.play_button.rect.collidepoint(mouse_pos) and not self.game_active:
+            self.stats.reset_stats()
+            self.game_active = True
+
+            self.bullets.empty()
+            self.aliens.empty()
+
+            self._create_fleet()
+            self.ship.center_ship()
+
+            pygame.mouse.set_visible(False)
 
 
 if __name__ == "__main__":
