f970bcf341daa58e44ff4c61a7c74bb2bfc81b4e
yanxinle <1020121123@qq.com>
2023-07-24 15:38:24 +0800

    添加 hello goodbye

---
+++ other/hello goodbye.py
@@ -0,0 +1,38 @@
+from colored import Fore
+
+from comm.common import slow_print
+
+slow_print(Fore.RED + '*        *       *********       *          *            *****', 0.002)
+slow_print(Fore.RED + Fore.YELLOW + '*        *       *               *          *          *       *', 0.002)
+slow_print(Fore.YELLOW + '*        *       *               *          *          *       *', 0.002)
+slow_print(Fore.GREEN + '**********       *********       *          *          *       *', 0.002)
+slow_print(Fore.GREEN + Fore.BLUE + '*        *       *               *          *          *       *', 0.002)
+slow_print(Fore.BLUE + '*        *       *               *          *          *       *', 0.002)
+slow_print(Fore.MAGENTA + '*        *       *********       ******     ******       *****', 0.002)
+print('')
+print('')
+print('')
+print('')
+print('')
+print('')
+slow_print(
+    Fore.RED + '  *******        ****         ****       ******             *****       *       *     *********',
+    0.001)
+slow_print(
+    Fore.GREEN + Fore.YELLOW + '*              *      *     *      *     *      *           *    *        *   *       *',
+    0.001)
+slow_print(
+    Fore.YELLOW + '*              *      *     *      *     *       *          *    *          *         *',
+    0.001)
+slow_print(
+    Fore.GREEN + '*   *******    *      *     *      *     *       *          *****           *         *********',
+    0.001)
+slow_print(
+    Fore.CYAN + '*       *      *      *     *      *     *       *          *    *          *         *',
+    0.001)
+slow_print(
+    Fore.BLUE + '*       *      *      *     *      *     *      *           *    *          *         *',
+    0.001)
+slow_print(
+    Fore.MAGENTA + '  *******        ****         ****       ******             *****           *         *********',
+    0.001)
