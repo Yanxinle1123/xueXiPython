964de93678ee31627b39d420ac7122f271c9c808
fanyan <fanyan@tencent.com>
2023-07-27 18:58:32 +0800

    #、增加加密也要输入密钥，并且每次对话只用输入一次

--- other/加密和解密3.py
+++ other/加密和解密3.py
@@ -5,11 +5,11 @@ from colored import Fore
 chengshu = random.randint(10, 60)
 
 
-def encrypt(text):
+def encrypt(text, miyao):
     encrypted_text = ""
     for char in text:
         code = ord(char)
-        encrypted_code = str(code * chengshu)
+        encrypted_code = str(code * miyao)
         encrypted_text += encrypted_code + ","
     return encrypted_text[:-1], encrypted_text.count(",")
 
@@ -26,28 +26,27 @@ def decrypt(text, miyao):
 def ead3():
     print(Fore.RGB(255, 170, 0) + '这是一个可以加密解密的程序')
     r = 1
+    print(Fore.RGB(255, 170, 0) + '本次程序密钥:', chengshu)
+    miyao = input('请输入密钥:')
     while True:
         print(Fore.RGB(125, 250, 85) +
               '----------------------------------------第 {} 次----------------------------------------'.format(r))
         huoqu = input('请输入要加密的内容(输入q退出):')
-
         if huoqu == 'q':
             print(Fore.RGB(255, 170, 0) + "已退出")
             break
         try:
-            encrypted_text, length = encrypt(huoqu)
-            print(Fore.RGB(225, 255, 0) + "密文长度:", length)
-            print(Fore.RGB(255, 170, 0) + '密钥:', chengshu)
+            encrypted_text, length = encrypt(huoqu, int(miyao))
             print(Fore.RGB(50, 150, 225) + "密文:", encrypted_text)
+            print(Fore.RGB(225, 255, 0) + "密文长度:", length)
+
             miwen = input(Fore.RGB(125, 250, 85) + "请输入要解密的内容(输入q退出): ")
             if miwen == 'q':
                 print(Fore.RGB(255, 170, 0) + "已退出")
                 break
-            miyao = input('请输入密钥:')
 
             decrypted_text = decrypt(miwen, int(miyao))
             print(Fore.RGB(225, 255, 0) + "明文:", decrypted_text)
-
             r = r + 1
         except:
             print(Fore.RGB(225, 0, 50) + "输入有误")
