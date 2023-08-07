5a21e542cba72e3d5ab08f0d3054015b383ef928
fanyan <fanyan@tencent.com>
2023-08-07 13:14:32 +0800

    更新 snake2

--- game/snake2.py
+++ game/snake2.py
@@ -1,10 +1,9 @@
 import random
 import sys
 
-import pyautogui
+import numpy as np
 import pygame
 
-a = pyautogui
 # Initialize pygame
 pygame.init()
 
@@ -16,6 +15,7 @@ pygame.display.set_caption("Snake Game")
 # Set up colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+GRAY = (200, 200, 200)
 
 
 def random_color():
@@ -23,12 +23,12 @@ def random_color():
 
 
 # Set up snake and food
-snake_pos = [[100, 50], [90, 50], [80, 50]]
-snake_speed = [10, 0]
+snake_pos = np.array([[100, 50], [90, 50], [80, 50]])
+snake_speed = np.array([10, 0])
 snake_colors = [random_color() for _ in range(len(snake_pos))]
-food_pos = [random.randrange(1, WIDTH // 10) * 10, random.randrange(1, HEIGHT // 10) * 10]
+food_pos = np.array([random.randrange(1, WIDTH // 10) * 10, random.randrange(1, HEIGHT // 10) * 10])
+food_speed = np.array([0, 0])
 food_color = random_color()
-food_spawn = True
 score = 0
 
 # Set up game clock
@@ -37,24 +37,124 @@ clock = pygame.time.Clock()
 # Set up font for score display
 font = pygame.font.Font(None, 36)
 
+# Initialize food path
+food_path = [food_pos.copy()]
+
+
+# Set up button
+class Button:
+    def __init__(self, x, y, width, height, color, text='', text_color=BLACK):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.color = color
+        self.text = text
+        self.text_color = text_color
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, self.rect)
+        if self.text:
+            font = pygame.font.Font(None, 20)
+            text = font.render(self.text, True, self.text_color)
+            text_rect = text.get_rect(center=self.rect.center)
+            surface.blit(text, text_rect)
+
+    def is_clicked(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                return True
+        return False
+
+
+button_x, button_y = 10, 60
+button_width, button_height = 200, 40
+button_color = (0, 255, 0)
+button_text = 'Toggle Auto/Manual'
+button_text_color = BLACK
+button = Button(button_x, button_y, button_width, button_height, button_color, button_text, button_text_color)
+
+# Set up flag for food movement mode
+auto_mode = True
+
 
 def draw_score():
     score_text = font.render("Score: " + str(score), True, BLACK)
     screen.blit(score_text, (10, 10))
 
 
-def move_towards_food():
-    if snake_pos[0][0] < food_pos[0]:
-        return [10, 0]
-    elif snake_pos[0][0] > food_pos[0]:
-        return [-10, 0]
-    elif snake_pos[0][1] < food_pos[1]:
-        return [0, 10]
-    elif snake_pos[0][1] > food_pos[1]:
-        return [0, -10]
+def generate_food():
+    global food_pos
+    while True:
+        food_pos = np.array([random.randrange(1, WIDTH // 10 - 1) * 10, random.randrange(1, HEIGHT // 10 - 1) * 10])
+        if food_pos[1] != snake_pos[0][1]:
+            break
 
 
-a.moveTo(600, 300)
+def increase_score():
+    global score
+    score += 1
+
+
+def move_snake():
+    global snake_pos
+    global snake_speed
+    global food_path
+
+    if auto_mode:
+        diff = food_pos - snake_pos[0]
+        snake_speed = np.sign(diff) * 10
+    else:
+        if len(food_path) > 0:
+            target_pos = food_path.pop(0)
+            snake_speed = (target_pos - snake_pos[0]) / 2  # 蛇速度是食物速度的一半
+        else:
+            snake_speed = np.array([0, 0])
+
+    snake_pos = np.insert(snake_pos, 0, snake_pos[0] + snake_speed, axis=0)
+    snake_colors.insert(0, random_color())
+
+    # 检查蛇头与食物之间的距离
+    distance = np.linalg.norm(snake_pos[0] - food_pos)
+
+    if distance <= 20:  # 当距离小于等于20时（允许一格距离），认为食物被吃掉
+        generate_food()
+        increase_score()
+    else:
+        snake_pos = snake_pos[:-1]
+        snake_colors.pop()
+
+
+def move_food():
+    global food_pos
+    global food_speed
+
+    if auto_mode:
+        diff = pygame.math.Vector2(snake_pos[0][0] - food_pos[0], snake_pos[0][1] - food_pos[1])
+        food_speed = -diff.normalize() * (10 / 3)  # 设置食物速度为蛇速度的三分之一
+    else:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            food_speed = np.array([-10, 0])  # 向左移动
+        elif keys[pygame.K_RIGHT]:
+            food_speed = np.array([10, 0])  # 向右移动
+        elif keys[pygame.K_UP]:
+            food_speed = np.array([0, -10])  # 向上移动
+        elif keys[pygame.K_DOWN]:
+            food_speed = np.array([0, 10])  # 向下移动
+        else:
+            food_speed = np.array([0, 0])  # 停止移动
+
+    new_food_pos = food_pos + food_speed
+    # 检查食物移动后是否与蛇身碰撞或超出边界
+    if (
+            new_food_pos[0] < 0
+            or new_food_pos[0] >= WIDTH
+            or new_food_pos[1] < 0
+            or new_food_pos[1] >= HEIGHT
+            or np.any(np.all(snake_pos == new_food_pos, axis=1))  # 避免与蛇身碰撞
+    ):
+        generate_food()
+    else:
+        food_pos = new_food_pos
+
 
 while True:
     # Handle events
@@ -69,26 +169,33 @@ while True:
         elif event.type == pygame.VIDEORESIZE:
             WIDTH, HEIGHT = event.w, event.h
             screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+        elif button.is_clicked(event):
+            auto_mode = not auto_mode
+            if auto_mode:
+                button.color = (0, 255, 0)
+                button.text = 'Toggle Auto/Manual'
+            else:
+                button.color = (255, 0, 0)
+                button.text = 'Toggle Auto/Manual'
 
     # Move snake
-    snake_speed = move_towards_food()
-    snake_pos.insert(0, [snake_pos[0][0] + snake_speed[0], snake_pos[0][1] + snake_speed[1]])
-    snake_colors.insert(0, random_color())
-    if snake_pos[0] == food_pos:
-        score += 1
-        food_spawn = False
-    else:
-        snake_pos.pop()
-        snake_colors.pop()
+    move_snake()
 
-    # Spawn food
-    if not food_spawn:
-        food_pos = [random.randrange(1, WIDTH // 10) * 10, random.randrange(1, HEIGHT // 10) * 10]
-        food_color = random_color()
-    food_spawn = True
+    # Move food
+    move_food()
 
     # Check for collisions with walls
-    if snake_pos[0][0] < 0 or snake_pos[0][0] >= WIDTH or snake_pos[0][1] < 0 or snake_pos[0][1] >= HEIGHT:
+    if (
+            snake_pos[0][0] < 0
+            or snake_pos[0][0] >= WIDTH
+            or snake_pos[0][1] < 0
+            or snake_pos[0][1] >= HEIGHT
+    ):
+        pygame.quit()
+        sys.exit()
+
+    # Check for collision with snake body
+    if np.all(snake_pos[0] == snake_pos[1:]):
         pygame.quit()
         sys.exit()
 
@@ -101,8 +208,11 @@ while True:
     # Draw score
     draw_score()
 
+    # Draw button
+    button.draw(screen)
+
     # Update display
     pygame.display.flip()
 
     # Cap framerate
-    clock.tick(100)  # Set the frame rate to the maximum value
+    clock.tick(15)
