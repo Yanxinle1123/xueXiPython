34c4ee36b70c7e4efab4b20d10d67a121ea70823
yanxinle <1020121123@qq.com>
2023-08-09 12:31:31 +0800

    更新 Moving Bubbles

--- game/Moving Bubbles.py
+++ game/Moving Bubbles.py
@@ -23,21 +23,21 @@ screen.fill(rgb_color)
 
 original_ball = pygame.image.load('/Users/lele/lele/3031903678.png')
 
-# 创建包含5个气泡的列表，每个气泡有一个随机大小和速度
+# 创建包含1个气泡的列表，每个气泡有一个随机大小和速度
 bubbles = []
 existing_rects = []
-for i in range(5):
-    ball_size = random.randint(50, 200)
+for i in range(1):
+    ball_size = random.randint(200, 200)
     ball = pygame.transform.scale(original_ball, (ball_size, ball_size))
     ballrect = get_non_overlapping_rect(existing_rects, ball_size, ball_size)
     existing_rects.append(ballrect)
-    speed_x = random.randint(10, 25)
-    speed_y = random.randint(10, 30)
-    bubbles[i] = [ball, ballrect, speed_x, speed_y]
+    speed_x = random.randint(1, 1)
+    speed_y = random.randint(1, 1)
+    bubbles.append([ball, ballrect, speed_x, speed_y])
 
 clock = pygame.time.Clock()
 pygame.display.set_caption(title)
-
+speed = 1
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -47,9 +47,19 @@ while True:
             width, height = event.w, event.h
             screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_EQUALS]:
+        speed += 1
+    elif keys[pygame.K_MINUS]:
+        speed -= 1
+    if speed >= 1101:
+        speed = 1100
+    elif speed <= 9:
+        speed = 10
+
     screen.fill(rgb_color)
 
-    # 更新每个气泡的位置并绘制到屏幕上
+    # 更新气泡的位置并绘制到屏幕上
     for i, (ball, ballrect, speed_x, speed_y) in enumerate(bubbles):
         ballrect = ballrect.move(speed_x, speed_y)
 
@@ -58,16 +68,10 @@ while True:
         if ballrect.top < 0 or ballrect.bottom > height:
             speed_y = -speed_y
 
-        # 检查气泡是否相交，如果相交，则更改运动方向
-        for j, (_, other_rect, other_speed_x, other_speed_y) in enumerate(bubbles):
-            if i != j and ballrect.colliderect(other_rect):
-                speed_x = -speed_x
-                speed_y = -speed_y
-
         # 更新气泡速度
         bubbles[i] = [ball, ballrect, speed_x, speed_y]
 
         screen.blit(ball, ballrect)
 
     pygame.display.flip()
-    clock.tick(30)
+    clock.tick(speed)
