e60f717d4e2baac908fb18fc0c6254118b2c7a95
yanxinle <1020121123@qq.com>
2023-08-14 17:07:56 +0800

    Explore the 'tkinter' module in the 'Python module' file.

--- Python module/tkinter/tkinter5.py
+++ Python module/tkinter/tkinter5.py
@@ -1,10 +1,12 @@
 import tkinter as tk
 
 
+# 退出
 def show_quit_button():
-    window.after(1000, window.quit())
+    window.quit()
 
 
+# 使窗口中的所有组件居中
 def center_widgets():
     window_width = window.winfo_width()
     window_height = window.winfo_height()
@@ -17,6 +19,7 @@ def center_widgets():
         widget.place(x=widget_x, y=widget_y)
 
 
+# 清空屏幕上的所有组件
 def clear_screen():
     for widget in window.winfo_children():
         widget.destroy()
@@ -25,29 +28,32 @@ def clear_screen():
     cleared_label = tk.Label(window, text="已清空按钮<Clear>和文字<清空测试>", font=('华文楷体', 100))
     cleared_label.pack()
 
-    # 在1秒后显示“退出”按钮并删除“已清空”文本
+    # 在1秒后退出
     window.after(1000, show_quit_button)
 
 
+# 关闭窗口
 def button_click():
-    window.destroy()
+    window.quit()
 
 
+# 初始化窗口
 window = tk.Tk()
 window.minsize(1720, 358)
 window.geometry('1720x358')
 window.title('Hello Tkinter')
 
-frame = tk.Frame(window)
+frame = tk.Frame(window)  # 创建一个 Frame 小部件
 
 label = tk.Label(frame, text="清空测试", font=('华文楷体', 100))
-label.pack()
+label.pack()  # 添加标签到 Frame
 
 button = tk.Button(frame, text="Clear", font=('华文楷体', 100), command=clear_screen)
-button.pack()
+button.pack()  # 添加按钮到 Frame
 
-frame.pack(expand=True)
+frame.pack(expand=True)  # 将 Frame 添加到窗口并使其可扩展
 
-window.bind("<Configure>", lambda event: center_widgets())  # 在窗口大小更改时调用 center_widgets 函数
+# 在窗口大小更改时调用 center_widgets 函数
+window.bind("<Configure>", lambda event: center_widgets())
 
-window.mainloop()
+window.mainloop()  # 运行主循环
---
+++ other/Tang Monk and Apprentice.py
@@ -0,0 +1,65 @@
+import tkinter as tk
+
+from PIL import Image, ImageTk
+
+
+def show(event):
+    ID = event.widget['text']
+    if ID == record[0]:
+        my_canvas.itemconfig(img_on_canvas, image=img0)
+    elif ID == record[1]:
+        my_canvas.itemconfig(img_on_canvas, image=img1)
+    elif ID == record[2]:
+        my_canvas.itemconfig(img_on_canvas, image=img2)
+    elif ID == record[3]:
+        my_canvas.itemconfig(img_on_canvas, image=img3)
+    center_widgets()
+
+
+def center_widgets(event=None):
+    window_width = root.winfo_width()
+    window_height = root.winfo_height()
+
+    text_x = window_width // 2
+    text_y = window_height // 4
+    my_canvas.coords(text_id, text_x, text_y)
+
+    img_x = window_width // 2
+    img_y = window_height // 2
+    my_canvas.coords(img_on_canvas, img_x, img_y)
+
+
+root = tk.Tk()
+root.title('唐僧师徒')
+root.geometry('1401x893')
+root.minsize(1401, 893)
+root.resizable(True, True)
+
+fm1 = tk.Frame(root)
+fm1.pack(side=tk.TOP, padx=10, pady=10)
+fm2 = tk.Frame(root)
+fm2.pack(fill=tk.BOTH, expand=True)
+my_canvas = tk.Canvas(fm2, bg='orange')
+my_canvas.pack(fill=tk.BOTH, expand=True)
+
+text_id = my_canvas.create_text(0, 0, text='唐僧师徒', font=('行楷-简', 340), fill='blue', anchor=tk.CENTER)
+
+record = ['唐三藏', '孙悟空', '猪八戒', '沙和尚']
+list = []
+
+img0 = ImageTk.PhotoImage(Image.open('/Users/lele/lele/3280671361.png'))
+img1 = ImageTk.PhotoImage(Image.open('/Users/lele/lele/386211393.png'))
+img2 = ImageTk.PhotoImage(Image.open('/Users/lele/lele/2939577441.png'))
+img3 = ImageTk.PhotoImage(Image.open('/Users/lele/lele/3191369692.png'))
+
+img_on_canvas = my_canvas.create_image(0, 0, anchor=tk.CENTER)
+
+button = tk.Button
+for i in range(4):
+    list.append(button(fm1, text=record[i], font=('华文楷体', 35), width=5, height=1))
+    list[i].pack(side=tk.LEFT, anchor=tk.NW)
+    list[i].bind('<ButtonRelease-1>', show)
+
+root.bind("<Configure>", center_widgets)
+
+root.mainloop()
