2a46a345210804c8ec680ae719d2ffabb817a3fe
fanyan <fanyan@tencent.com>
2023-08-16 13:42:21 +0800

    #、找到一个英语翻译网站

--- Python module/Python crawler/crawler4.py
+++ Python module/Python crawler/crawler4.py
@@ -3,16 +3,16 @@ from bs4 import BeautifulSoup
 
 # 发送HTTP请求，获取网页内容
 fanyi = 'hello'
-url = f'https://fanyi.baidu.com/#auto/zh/{fanyi}'
+url = f'https://cn.linguee.com/%E4%B8%AD%E6%96%87-%E8%8B%B1%E8%AF%AD/search?source=%E8%8B%B1%E8%AF%AD&query=/{fanyi}'
 response = requests.get(url)
 html_content = response.content
-print(url)
+print(f'url={url}')
 # 解析HTML网页
 soup = BeautifulSoup(html_content, 'html.parser')
-fruit_list = soup.find('div', {'class': "main main-outer"}).find('div', {'class': 'main main-inner'}).find('div', {
-    'class': 'inner'}).find('div', {'class': 'translate-wrap'}).find('div', {'class': 'translateio'}).find('div', {
-    'class': 'translate-main clearfix'}).find('div', {'class': 'trans-right'}).find('div',
-                                                                                    {'class': 'output-wrap'}).find(
-    'div', {'class': 'output-mod ordinary-wrap'}).find('div', {'class': 'output-bd'}).find('p', {
-    'class': "ordinary-output target-output clearfix"})
-print(fruit_list)
+fruit_list = soup.find_all('a', class_='dictLink featured')
+
+print(f'fruit_list={fruit_list}')
+
+# 遍历解析
+for fruit in fruit_list:
+    print(f'one_text={fruit.text}')
