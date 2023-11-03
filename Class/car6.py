b31c1e882ab73bbd02e9bd7dd9799a253eb8e813
yanxinle <1020121123@qq.com>
2023-11-03 13:44:23 +0800

    研究文件的读取

--- Class/car6.py
+++ Class/car6.py
@@ -1,3 +1,7 @@
 import car2
 
 mustang = car2.Car('ford', 'mustang', 2024)
+print(mustang.get_descriptive_name())
+
+leaf = car2.ElectricCar('nissan', 'leaf', 2024)
+print(leaf.get_descriptive_name())
